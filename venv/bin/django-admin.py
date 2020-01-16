bb6272958a309c7e3c5ca726100d85179e21c5f9
Valentyn <valentyn@Valentyns-MacBook-Pro.local>
2020-01-16 20:29:10 +0200

    added some models

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/valentyn/Desktop/some_ex/training_tasks/first_site_django/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
